d967a21eb970a67ec646f44d9efd744b42310759
momegas <megaklis.vasilakis@gmail.com>
2023-10-31 18:29:17 +0200

    Add more documentation

--- src/lib/public-api/openapi.ts
+++ src/lib/public-api/openapi.ts
@@ -72,8 +72,8 @@ publicAPIRegistry.registerPath({
   method: 'get',
   path: '/indexes',
   tags: ['Indexes'],
-  description: 'Get all indexes',
   summary: 'Get all indexes',
+  description: 'Get all indexes of the current project.',
   security: [{ [bearerAuth.name]: [] }],
   responses: {
     ...response(GetIndexesResponse, 'Array with all the indexes'),
@@ -85,8 +85,8 @@ publicAPIRegistry.registerPath({
   method: 'post',
   path: '/indexes',
   tags: ['Indexes'],
-  description: 'Create new index',
   summary: 'Create new index',
+  description: 'Create new index under the current project.',
   security: [{ [bearerAuth.name]: [] }],
   request: request(CreateIndexRequest, 'The index name'),
   responses: {
@@ -99,10 +99,12 @@ publicAPIRegistry.registerPath({
   method: 'post',
   path: '/indexes/search',
   tags: ['Indexes'],
-  description: 'Contextually search an index',
-  summary: 'Contextually search an index',
+  summary: 'Search for similar documents',
+  description:
+    'Contextually search an index for similar documents. The search is done by comparing the embeddings of the documents using cosine similarity.',
   security: [{ [bearerAuth.name]: [] }],
-  // request: request(Do, 'The index name'),
+  // TODO:
+  // request: request(____, ''),
   responses: {
     ...response(SimilarDocumentsResponse, 'Similar documents found'),
     ...errorResponse,
@@ -113,8 +115,9 @@ publicAPIRegistry.registerPath({
   method: 'post',
   path: '/documents',
   tags: ['Documents'],
-  description: 'Load a document into an index.',
-  summary: 'Load a document into an index.',
+  summary: 'Load document',
+  description:
+    'Load a document into an index. Before the document is loaded, SquareDev creates embeddings for the document so that it can be contextually searched later.',
   security: [{ [bearerAuth.name]: [] }],
   request: request(CreateDocumentRequest, 'The new document'),
   responses: {
@@ -127,9 +130,8 @@ publicAPIRegistry.registerPath({
   method: 'post',
   path: '/chat/completions',
   tags: ['Language models'],
+  summary: 'Chat',
   description:
-    'Given a prompt, the model will return one or more predicted completions',
-  summary:
     'Given a prompt, the model will return one or more predicted completions',
   security: [{ [bearerAuth.name]: [] }],
   request: request(ChatCompletionRequest, 'The new document'),
@@ -143,10 +145,9 @@ publicAPIRegistry.registerPath({
   method: 'post',
   path: '/chat/rag',
   tags: ['Language models'],
+  summary: 'Retrieval augmented generation (RAG)',
   description:
-    'Given a prompt, the model will return one or more predicted completions',
-  summary:
-    'Given a prompt, the model will return one or more predicted completions',
+    'SquareDev contextually searches for related documents in the specified index and uses them to improve the generation of the completion.',
   security: [{ [bearerAuth.name]: [] }],
   request: request(RagCompletionRequest, 'The retrieval request object.'),
   responses: {
@@ -170,8 +171,29 @@ export function generateOpenApi() {
       },
       tags: [
         {
+          name: 'Authorization',
+          description: `Authorization is done via a Bearer token in the Authorization header. Your API token can be found in your project via the studio.          
+          `,
+        },
+        {
           name: 'Indexes',
-          description: 'string',
+          description: `Indexes are the main way to store and organize documents. 
+          They contain the documents, their content, embeddings and metadata.`,
+        },
+        {
+          name: 'Documents',
+          description: `Documents are the main way content is stored in SquareDev. They are stored in Indexes and contain the content, embeddings and metadata.
+            Usually documents are created from files or unstructured data. After a file is uploaded in SquareDev platform it is splitted into documents,
+            and these documents along with their embeddings (vector representations of the content) are stored in the index.
+            After that documents can be searched and retrieved contextually.
+          `,
+        },
+        {
+          name: 'Language models',
+          description: `Language model endpoints are the main way to interact with a LLMs. They can be used to generate simple completions from an input, 
+          or combined with documents for retrieval augmented generation (RAG). The RAG endpoint can be used to generate completions from a prompt, that
+          are also context aware from the documents in the index that best answer the prompt (user question).
+          .`,
         },
       ],
       servers: [{ url: 'api' }],
